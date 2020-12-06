5bd2e094de84b30a80022ce8c652d7394496cc75
Bereketab Gulai <biruktes@outlook.com>
2020-12-06 22:43:35 +0000

    Update db and setting

--- bug_tracker/bug_tracker/settings.py
+++ bug_tracker/bug_tracker/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'bug_tracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'SSTS_Bug_Tracker_DB',
+        'NAME': os.path.join(BASE_DIR, 'SSTS_Bug_Tracker_DB'),
     }
 }
 # else:
