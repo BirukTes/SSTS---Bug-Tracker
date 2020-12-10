84a8af376a4b0264a0ffc76a52401515051460bc
Bereketab Gulai <biruktes@outlook.com>
2020-12-10 23:19:28 +0000

    Update settings to remove secret key

--- bug_tracker/bug_tracker/settings.py
+++ bug_tracker/bug_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import secrets
 import sys
 from pathlib import Path
 import django_heroku
@@ -23,7 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c9ho_i__c7(00anf7amgms&(+l6nsqn=4e0gmk5w#c4ka76yca'
+if sys.argv[1] != 'runserver':
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    from .secret_settings import *
+    SECRET_KEY = getSec()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if sys.argv[1] != 'runserver':
